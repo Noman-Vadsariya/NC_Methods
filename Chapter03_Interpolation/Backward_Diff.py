a18fc14df244fb45e3eca21242e99403590450c5
Noman-Vadsariya <noman.nooruddin02@gmail.com>
2021-05-02 23:37:48 +0500

    Forward and Backward Difference Done

---
+++ Chapter03_Interpolation/Backward_Diff.py
@@ -0,0 +1,66 @@
+class Forward_Difference:
+    
+    def __init__(self,x_data,y_data):
+
+        self.x_data = x_data
+        self.y_data = y_data
+    
+    #returns factorial of any number
+    def factorial(self,n):
+        if n == 1:
+            return 1
+
+        return n*self.factorial(n-1)
+    
+    def get_S(self,S,n):
+        t = S
+        
+        for i in range(1, n):
+            t = t * (S + i)
+
+        return t
+
+    def Construct_Forward_Difference_Table(self):
+
+        for i in range(1, len(self.x_data)):
+            for j in range(len(self.x_data)-1,i-1,-1):
+                self.y_data[j][i] = self.y_data[j][i - 1] - self.y_data[j - 1][i - 1]
+
+    # Displaying the forward difference table
+    def print_table(self):
+        
+        for i in range(len(self.x_data)):
+            print(self.x_data[i], end = "\t")
+
+            for j in range(0,i+1):
+                print(round(self.y_data[i][j],7), end = "\t")
+            
+            print("")
+    
+    def interpolate(self,x):
+
+        sum = self.y_data[len(self.x_data)-1][0]  #since forward difference that's why yn
+
+        S = (x - self.x_data[len(self.x_data)-1]) / (self.x_data[1] - self.x_data[0])
+
+        for i in range(1,len(self.x_data)):
+            sum = sum + (self.get_S(S, i) * self.y_data[len(self.x_data)-1][i]) / self.factorial(i)
+        
+        return sum
+
+n = 5
+x = [ 1891, 1901, 1911,  1921, 1931 ]            
+y = [[0 for i in range(n)]
+        for j in range(n)]
+
+y[0][0] = 46
+y[1][0] = 66
+y[2][0] = 81
+y[3][0] = 93
+y[4][0] = 101
+
+fd = Forward_Difference(x, y)
+fd.Construct_Forward_Difference_Table()
+fd.print_table()
+print()
+print(f'Interpolated Value: {round(fd.interpolate(1925),5)}')
---
+++ Chapter03_Interpolation/Forward_Diff.py
@@ -0,0 +1,65 @@
+class Forward_Difference:
+    
+    def __init__(self,x_data,y_data):
+
+        self.x_data = x_data
+        self.y_data = y_data
+    
+    #returns factorial of any number
+    def factorial(self,n):
+        if n == 1:
+            return 1
+
+        return n*self.factorial(n-1)
+    
+    def get_S(self,S,n):
+        t = S
+        
+        for i in range(1, n):
+            t = t * (S - i)
+
+        return t
+
+    def Construct_Forward_Difference_Table(self):
+        for i in range(1, len(self.x_data)):
+            for j in range(len(self.x_data) - i):
+                self.y_data[j][i] = self.y_data[j + 1][i - 1] - self.y_data[j][i - 1]
+
+    # Displaying the forward difference table
+    def print_table(self):
+        
+        for i in range(len(self.x_data)):
+            print(self.x_data[i], end = "\t")
+
+            for j in range(len(self.x_data) - i):
+                print(round(self.y_data[i][j],7), end = "\t")
+            
+            print("")
+    
+    def interpolate(self,x):
+
+        sum = self.y_data[0][0]  #since forward difference that's why y0
+
+        S = (x - self.x_data[0]) / (self.x_data[1] - self.x_data[0])
+
+        for i in range(1,len(self.x_data)):
+            sum = sum + (self.get_S(S, i) * self.y_data[0][i]) / self.factorial(i)
+        
+        return sum
+
+n = 5
+x = [0,0.2,0.4,0.6,0.8]
+y = [[0 for i in range(n)]
+        for j in range(n)]
+
+y[0][0] = 1
+y[1][0] = 1.22140
+y[2][0] = 1.49182
+y[3][0] = 1.82212
+y[4][0] = 2.22554
+
+fd = Forward_Difference(x, y)
+fd.Construct_Forward_Difference_Table()
+fd.print_table()
+print()
+print(f'Interpolated Value: {round(fd.interpolate(0.05),5)}')
