4e66d07b9bda465d683f007865ec6b86deefd485
SubhanKhan-458 <codephobia458@gmail.com>
2021-05-26 00:24:40 -0700

    Integration done

--- Chapter04_Diff_Int/Integration/Trapezium_method.py
+++ Chapter04_Diff_Int/Integration/Trapezium_method.py
@@ -1,8 +1,30 @@
 from math import *
-import Functions
+import numpy as np
 
-def  main():
+sin = np.sin
+cos = np.cos
+tan = np.tan
+pi  = np.pi
+exp = np.exp
+ln  = np.log
+log = np.log10
+
+class Trapezium:
+	def __init__(self, f, a, b):
+		self.f = f
+		self.a = a
+		self.b = b
+	def iterations(self):
+		s  = (f(a) + f(b))/2.0
+		h = float((b - a)/n) 
 			
+		for i in range(1,n):
+			s = s + f(a + i*h)
+		area = s*h   
+		print('The Area is :',area)
+     		
+if __name__ == "__main__":
+	# Input Format: 3*x+2:
 	fx = input("Enter the function whose integral you wish to determine :")      
 	f  = lambda x: eval(fx)                                                     
 
@@ -10,14 +32,5 @@ def  main():
 	a = float(input('Please provide the lower limit:')); 
 	b = float(input('Please provide the upper limit:')); 
 
-	s  = (f(a) + f(b))/2.0
-	h = float((b - a)/n) 
-		
-	for i in range(1,n):
-		s = s + f(a + i*h)
-	area = s*h   
-
-	print('The Area is :',area)
-
-if __name__ == "__main__":
-	main()
+	obj = Trapezium(f, a, b)
+	obj.iterations()
