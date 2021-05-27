283a829f5788afbefc85a5b9e01c72725b79c755
Noman-Vadsariya <noman.nooruddin02@gmail.com>
2021-05-27 21:19:22 +0500

    Error Bound and Abs Error Done in 3 Point

--- Chapter04_Diff_Int/Differentiation/3point.py
+++ Chapter04_Diff_Int/Differentiation/3point.py
@@ -2,15 +2,16 @@ from sympy import *
 
 class ThreePoint:
     
-    def __init__(self,x_data,y_data,h):
+    def __init__(self,x_data,y_data,h,f):
         self.x_data = x_data
         self.y_data = y_data
         self.used_points = None
         self.error_bounds = []
         self.diff_data = []
+        self.absError = []
         self.h = h
         self.x = symbols('x')
-        self.fx = sympify('2*ln(x)**2 + 3*sin(x)')
+        self.fx = sympify(f)
         self.diffedFx = None
         self.isMidpoint = False
 
@@ -41,6 +42,13 @@ class ThreePoint:
             w = (((self.h ** 2) / 3) * self.getLargestDerivate())
 
         self.error_bounds.append(w)
+    
+    def getAbsoluteError(self):
+        d = diff(self.fx, self.x, 1)
+        
+        for i in range(len(self.x_data)):
+            Actual = d.subs(self.x, self.x_data[i])
+            self.absError.append(abs(Actual-self.diff_data[i]))
 
     def ThreePoint_Midpoint(self,index):
         a = self.y_data[index+1]-self.y_data[index-1]
@@ -73,23 +81,50 @@ class ThreePoint:
 
             self.getErrorBound()
         
+        self.getAbsoluteError()
+
     def get_Diff_data(self):
         return self.diff_data
 
     def print_table(self):
 
-        print("\nX\t\tF(y)\t\tF'(y)\n")
+        print()
+        print('{:<10}{:<16}{:<16}{:<16}{:<16}'.format("X","F(x)","F'(X)","Absolute Error","Error Bound"))
         for i in range(len(self.x_data)):
-            print(f"{round(self.x_data[i],2)}\t\t{round(self.y_data[i],7)}\t{round(self.diff_data[i],7)}\t{self.error_bounds[i]}")
+            print('{:<10}{:<16}{:<16}{:<16}{:<16}'.format(round(self.x_data[i],2),round(self.y_data[i],7),round(self.diff_data[i],7),round(self.absError[i],7),round(self.error_bounds[i],7)))
 
 
+if __name__ == "__main__":
+
+    print("Enter No Interpolating Points: ",end="")
+    no_points = int(input())
+
+    x_data = []
+    y_data = []
+    
+    print("\nEnter Interpolating Points: ")
+    for i in range(0,no_points):
+        print(f"x{i} : ",end="")
+        x = float(input())
+        x_data.append(x)
+        print(f"y{i} : ",end="")
+        y = float(input())
+        y_data.append(y)
+        print()
+
+    print("Enter Function: ",end="")
+    f = input()
+    f = f.replace("^","**")
+    
+    t = ThreePoint(x_data, y_data, 0.1,f)
+    t.Differentiate()
+    t.print_table()
 
 #Validation Required
 #Atleast 3 points required
 
-x = [2,2.1,2.2,2.3]
-y = [3.6887983,3.6905701,3.6688192,3.6245909]
+# x = [2,2.1,2.2,2.3]
+# y = [3.6887983,3.6905701,3.6688192,3.6245909]
 
-t = ThreePoint(x, y, 0.1)
-t.Differentiate()
-t.print_table()
+
+# '2*ln(x)**2 + 3*sin(x)'
